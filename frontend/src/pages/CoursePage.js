306f131542f1005842224ee1a030eda7f5df7208
Rebekah Chow <rebekah.chow@student.unsw.edu.au>
2021-06-22 10:25:45 +1000

    Add react-router Switch and add links to sidebar buttons

--- frontend/src/pages/CoursePage.js
+++ frontend/src/pages/CoursePage.js
@@ -2,11 +2,40 @@ import React, { useState } from "react"
 import Header from "../components/Header.js"
 import Sidebar from "../components/Sidebar.js"
 import WidgetsBar from "../components/WidgetsBar.js"
+import ForumOverviewPage from "../pages/ForumOverviewPage.js"
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect,
+} from "react-router-dom"
 import { Stack, Skeleton, useBreakpointValue, Flex, Container } from "@chakra-ui/react"
 
 function CoursePage() {
     //currently hardcoded sidebar content
-    const links = ["Home", "Course Outline", "Content", "Forums", "Support"]
+    // const links = ["Home", "Course Outline", "Content", "Forums", "Support"]
+    const links = [
+        {
+            name: 'Home',
+            url: '/',
+        },
+        {
+            name: 'Course Outline',
+            url: '/course-outline',
+        },
+        {
+            name: 'Content',
+            url: '/content',
+        },
+        {
+            name: 'Forums',
+            url: '/forums',
+        },
+        {
+            name: 'Support',
+            url: '/support',
+        }
+    ]
     const smVariant = 'drawer'
     const mdVariant = 'sidebar'
     const variants = useBreakpointValue({ base: smVariant, md: mdVariant })
@@ -18,18 +47,21 @@ function CoursePage() {
     //CHANGE WHATEVER IS IN THE CONTAINER IN LINE 25-29
     //CURRENTLY IS A SKELETON BOX
     return (
-        <div>
+        <Router>
             <Header sideBarLinks={links} setOpen={setOpen}></Header>
             <Flex>
                 <Sidebar links={links} isOpen={isOpen} setOpen={setOpen} variant={variants}></Sidebar>
                 <Container marginTop={50} maxWidth="100%">
-                    <Stack spacing={50}>
+                    {/* <Stack spacing={50}>
                         {[...Array(3).keys()].map(e => { counter += 1; return <Skeleton key={"feed_" + counter} height="300px" ></Skeleton>})}
-                    </Stack>
+                    </Stack> */}
+                    <Switch>
+                        <Route exact path="/forums"><ForumOverviewPage /></Route>
+                    </Switch>
                 </Container>
                 <WidgetsBar></WidgetsBar>
             </Flex>
-        </div>
+        </Router>
     )
 }
 
