0b45d1c7ed9812ea1d6c7539c1df7f82cffb3f7f
Rebekah Chow <rebekah.chow@student.unsw.edu.au>
2021-06-27 12:03:19 +1000

    Responsive styles for forum post page

--- frontend/src/components/forums/PostDetails.js
+++ frontend/src/components/forums/PostDetails.js
@@ -4,13 +4,14 @@ import {
     Box,
     Divider,
     Flex,
-    Text,
 } from "@chakra-ui/react"
 import { GrEdit, GrShare } from 'react-icons/gr'
+import AuthorDetails from './AuthorDetails'
 
 function PostDetails() {
     return (
-        <Box width={{ base: '100%', md: '80%' }} mt="24px" mx="auto" p="16px" borderRadius="8px" border="1px" borderColor="gray.300">
+        <Box width={{ base: '100%', lg: '80%' }} mt="24px" mx="auto" p="16px" borderRadius="8px" border="1px" borderColor="gray.300">
+            <AuthorDetails />
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
             veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
@@ -20,9 +21,6 @@ function PostDetails() {
                 <Flex>
                     <Button pr="8px" leftIcon={<GrShare />} />
                     <Button ml="8px" pr="8px" leftIcon={<GrEdit />} /> {/*  ONLY SHOW THIS IF USER IS AUTHOR OF POST */}
-                </Flex>
-                <Flex alignItems="center">
-                    <Text h="24px">Author and Date</Text>
                 </Flex>
             </Flex>
         </Box>
--- frontend/src/components/forums/Responses.js
+++ frontend/src/components/forums/Responses.js
@@ -9,16 +9,20 @@ import {
     InputGroup,
 } from "@chakra-ui/react"
 import { AiOutlineSend } from 'react-icons/ai'
+import AuthorControls from './AuthorControls'
+import AuthorDetails from './AuthorDetails'
 
 function Responses() {
     return (
-        <Box width={{ base: '100%', md: '80%' }} mt="24px" mx="auto" p="16px" borderRadius="8px" border="1px" borderColor="gray.300">
+        <Box width={{ base: '100%', lg: '80%' }} mt="24px" mx="auto" p="16px" borderRadius="8px" border="1px" borderColor="gray.300">
             <Heading size="md" mb="12px" textTransform="uppercase">Responses</Heading>
+            <AuthorDetails />
             {/* if no response - "no response yet" */}
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
             veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
             commodo consequat.
+            <AuthorControls />
             {/* if no response and user is staff */}
             <Divider my="16px" />
             <form>
