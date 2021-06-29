1f480621eba53e98444f970cc0cede279d1b9ac7
Rebekah Chow <rebekah.chow@student.unsw.edu.au>
2021-06-29 12:21:06 +1000

    Users can share posts

--- frontend/src/components/forums/PostDetails/PostDetails.js
+++ frontend/src/components/forums/PostDetails/PostDetails.js
@@ -1,16 +1,30 @@
-import React from "react"
+import React, { useState } from "react"
 import {
     Button,
     Box,
     Divider,
     Flex,
     Text,
+    useToast,
 } from "@chakra-ui/react"
 import { GrEdit, GrShare } from 'react-icons/gr'
 import AuthorDetails from '../AuthorDetails'
 import styles from './PostDetails.module.css'
 
 function PostDetails({ post: { author, published_date, description }}) {
+    const toast = useToast()
+
+    const shareLink = () => {
+        const link = window.location.href
+        navigator.clipboard.writeText(link)
+        toast({
+            title: 'Copied link',
+            status: 'success',
+            duration: 3000,
+            isClosable: true,
+        })
+    }
+
     return (
         <Box width={{ base: '100%', lg: '80%' }} mt="24px" mx="auto" p="16px" borderRadius="8px" border="1px" borderColor="gray.300">
             <AuthorDetails author={author} date={published_date} />
@@ -18,7 +32,7 @@ function PostDetails({ post: { author, published_date, description }}) {
             <Divider my="16px" />
             <Flex justifyContent="space-between">
                 <Flex>
-                    <Button pr="8px" leftIcon={<GrShare />} />
+                    <Button pr="8px" leftIcon={<GrShare />} onClick={shareLink} />
                     <Button ml="8px" pr="8px" leftIcon={<GrEdit />} /> {/*  ONLY SHOW THIS IF USER IS AUTHOR OF POST */}
                 </Flex>
             </Flex>
