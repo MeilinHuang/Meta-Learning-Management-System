3b6e0fe118d33a2a1f3376a1412ff7331307cab5
Rebekah Chow <rebekah.chow@student.unsw.edu.au>
2021-07-12 14:45:11 +1000

    Users can search posts

--- frontend/src/pages/ForumOverviewPage.js
+++ frontend/src/pages/ForumOverviewPage.js
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from "react"
 import { 
+    Box,
     Button,
     Center,
     Flex,
@@ -21,6 +22,7 @@ function ForumOverviewPage() {
     const context = useContext(StoreContext)
     const { posts: [posts, setPosts], showPinned: [showPinned, setShowPinned] } = context;
     const [pinnedPosts, setPinnedPosts] = useState([])
+    const [searchTerm, setSearchTerm] = useState('')
 
     useEffect(() => {
         fetch('http://localhost:8000/forum').then(r => r.json()).then(data => setPosts(data))
@@ -34,16 +36,36 @@ function ForumOverviewPage() {
     const buttonIcon = useBreakpointValue({ base: <GrAdd />, md: null })
     const { isOpen, onOpen, onClose } = useDisclosure()
 
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        if (searchTerm === '') {
+            fetch('http://localhost:8000/forum').then(r => r.json()).then(data => {
+                setPosts(data)
+                setShowPinned(true)
+            })
+            return
+        }
+
+        fetch(`http://localhost:8000/forum/search/${searchTerm.toLowerCase()}`).then(r => r.json()).then(data => {
+            setPosts(data)
+            setShowPinned(false)
+        })
+        console.log(searchTerm)
+    }
+
     return (
         <>
             <Flex justify="center">
                 <Center width={{ base: '100%', lg: '80%' }}>
                     <Button onClick={onOpen} leftIcon={buttonIcon} pr={{ base: '8px', md: '16px' }}>{buttonContents}</Button>
                     <AddPostModal isOpen={isOpen} onClose={onClose} />
-                    <InputGroup variant="filled" ml={{ base: '16px', md: '24px'}}>
-                        <InputLeftElement pointerEvents="none" children={<SearchIcon color="gray.300" />}/>
-                        <Input placeholder="Search"></Input>
-                    </InputGroup>
+                    <Box as="form" onSubmit={handleSubmit} width="100%" ml={{ base: '16px', md: '24px'}}>
+                        <InputGroup variant="outline">
+                            <InputLeftElement pointerEvents="none" children={<SearchIcon color="gray.300" />}/>
+                            <Input placeholder="Search" onChange={e => setSearchTerm(e.target.value)}></Input>
+                        </InputGroup>
+                    </Box>
                 </Center>
             </Flex>
             <Filter />
