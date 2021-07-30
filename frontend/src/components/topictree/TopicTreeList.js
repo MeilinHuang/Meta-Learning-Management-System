2e4adb4d3982646ce517503fb7f65374c9f888a3
Allen <allenwu564@hotmail.com>
2021-07-30 22:19:09 +1000

    updated list view ui

--- frontend/src/components/topictree/TopicTreeList.js
+++ frontend/src/components/topictree/TopicTreeList.js
@@ -1,90 +1,125 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { 
     Heading,
     Box,
-    Stack,
     Flex,
-    Divider,
     Button,
     Text,
     InputGroup,
     InputLeftElement,
-    Input
+    Input,
+    Accordion, 
+    AccordionItem, 
+    AccordionButton, 
+    AccordionPanel,
+    AccordionIcon,
+    Stack,
+    Divider,
+    textDecoration
 } from '@chakra-ui/react';
-import { ArrowRightIcon, SearchIcon } from '@chakra-ui/icons';
-import { backend_url, topic_group_url } from '../../Constants.js';
+import { SearchIcon } from '@chakra-ui/icons';
+import { topic_group_url } from '../../Constants.js';
 import { Spinner } from '@chakra-ui/spinner';
 import TopicTreeHeader from "./TopicTreeHeader.js";
 import { useHistory } from "react-router-dom";
 
 export default function TopicTreeList() {
     const [data, setData] = useState([]);
+    const [display, setDisplay] = useState([])
     const [view, setView] = useState("List View");
-    const history = useHistory();
 
     useEffect(async function () {
         fetch(topic_group_url)
         .then(response => response.json())
         .then(function (response) {
-            console.log('response', response);
             setData(response);
+            setDisplay(response)
         });
     }, []);
 
     let pageView = null;
     if (data != null) {
+        console.log(data)
         //Data is a list of topic groups
         pageView = (
-        <div>
+        <Box>
             <TopicTreeHeader id="topic-tree-header" view={view}></TopicTreeHeader>
-            <Box paddingInline={[5, 15, 30]} paddingBlock={10}>
-                <Flex flexDirection={["column", "column", "row"]}>
-                    <Heading>Topic Groups</Heading>
-                    <InputGroup variant="filled" marginLeft={["0", "0", "20%"]} width={["80%", "70%", "30%"]} alignSelf="center">
-                        <InputLeftElement pointerEvents="none" children={<SearchIcon color="gray.300" />}/>
-                        <Input placeholder="Search"></Input>
-                    </InputGroup>
+            <Flex paddingBlock={10} justifyContent="center" flexDirection="column">
+                <Flex justifyContent="center">
+                    <Flex width="50%">
+                        <InputGroup variant="filled" alignSelf="center">
+                            <InputLeftElement pointerEvents="none" children={<SearchIcon color="gray.300" />}/>
+                            <Input placeholder="Search" onChange={e => {
+                                let tempArray = []
+                                const target = e.target.value.toLowerCase()
+                                tempArray = data.filter(group => {
+                                    if (group.name.toLowerCase().indexOf(target) !== -1 || group.topic_code.toLowerCase().indexOf(target) !== -1) {
+                                        return true
+                                    }
+                                    for (let topic of group.topics_list) {
+                                        if (topic.name.toLowerCase().indexOf(target) !== -1) {
+                                            return true
+                                        }
+                                    }
+                                    return false
+                                })
+                                setDisplay(tempArray)
+                            }}></Input>
+                        </InputGroup>
+                        <Button bg="blue.400" color="white" _hover={{color:"black", bg:"blue.100"}} marginLeft={5}>
+                            ADD GROUP
+                        </Button>
+                    </Flex>
                 </Flex>
-                <Stack spacing={5} divider={<Divider></Divider>} marginTop={10}>
-                    {data.map(e => {
-                        let num_topics = e.topics_list.length + " topics"
-                        if (e.topics_list.length == 1) {
-                            num_topics.substring(0, num_topics.length - 1)
-                        }
-                        //TODO add links to topics when user clicks on topic group
-                        // also direct to course page if user clicks on visit course page button
-                        // could also show prerequesite topic groups
-                        return (
-                            <Flex key={"topic-group-" + e.id} padding={5} justifyContent="auto">
-                                <Button as={Flex} bg="white" cursor="pointer" flexGrow={1}>
-                                    <ArrowRightIcon color="blue.500" alignSelf="center" display={["none", "block"]} marginRight={10}></ArrowRightIcon>
-                                    <Flex flexDirection={["column", "column", "row"]}>
-                                        <Box marginLeft={10} width={[300]}>
-                                            <Heading fontSize="lg">
-                                                {e.name}
-                                            </Heading>
-                                        </Box>
-                                        <Box>
-                                            <Text>
-                                                {e.topic_code}
-                                            </Text>
-                                        </Box>
-                                    </Flex>
-                                    <Box marginLeft={10} fontSize="sm" display={["none", "none", "block"]}>
-                                        <Text>
-                                            {num_topics}
-                                        </Text>
-                                    </Box>
-                                    <Box flexGrow={1}></Box>
-                                </Button>
-                                <Button bg="blue.500" color="white">Course Page</Button>
-                            </Flex>
-                        )
-                    })}
-                </Stack>
-            </Box>
-            
-        </div>
+                <Flex justifyContent="center" marginTop={10}>
+                    <Accordion allowMultiple width="50%">
+                        {display.map(e => {
+                            return (
+                                <AccordionItem key={"topic-group-" + e.id}>
+                                    <AccordionButton as={Flex} bg="white" cursor="pointer">
+                                        <Flex flexDirection={["column", "column", "row"]}>
+                                            <Box marginRight={20} textOverflow="ellipsis">
+                                                <Heading fontSize="lg" width="200px" overflow="hidden" height="20px" textOverflow="ellipsis" whiteSpace="nowrap">
+                                                    {e.name}
+                                                </Heading>
+                                            </Box>
+                                            <Box>
+                                                <Text>
+                                                    {e.topic_code}
+                                                </Text>
+                                            </Box>
+                                        </Flex>
+                                        <Flex flexGrow={1}></Flex>
+                                        <AccordionIcon></AccordionIcon>
+                                    </AccordionButton>
+                                    <AccordionPanel>
+                                        <Stack>
+                                        {
+                                            e.topics_list.map(topic => {
+                                                return (
+                                                    <Flex key={e.name + "-topic-" + topic.name } justifyContent="flex-start" padding={5} _hover={{bg:"gray.100", fontWeight:"medium"}}>
+                                                        <Flex width="100%" cursor="pointer">
+                                                            <Text>
+                                                                {topic.name}
+                                                            </Text>
+                                                        </Flex>
+                                                    </Flex>
+                                                )
+                                            })
+                                        }
+                                        </Stack>
+                                        <Flex>
+                                            <Flex flexGrow={1}></Flex>
+                                            <Button bg="blue.400" color="white" _hover={{color:"black", bg:"blue.100"}}>ADD TOPIC</Button>
+                                        </Flex>
+                                    </AccordionPanel>
+                                </AccordionItem>
+                            )
+                        })}
+                    </Accordion>
+                </Flex>
+            </Flex>      
+        </Box>
         )
     }
     else {
