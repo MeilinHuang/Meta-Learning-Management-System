3528e26908f5f5a59517fed261ce896488a1bbf3
Allen <allenwu564@hotmail.com>
2021-08-02 17:26:14 +1000

    added modal for adding topic, topic groups and viewing topics

--- frontend/src/components/topictree/TopicTreeList.js
+++ frontend/src/components/topictree/TopicTreeList.js
@@ -15,18 +15,121 @@ import {
     AccordionIcon,
     Stack,
     Divider,
-    textDecoration
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    useDisclosure,
+    useToast
 } from '@chakra-ui/react';
 import { SearchIcon } from '@chakra-ui/icons';
-import { topic_group_url } from '../../Constants.js';
+import { topic_group_url, post_new_topic_url, get_topic_group } from '../../Constants.js';
 import { Spinner } from '@chakra-ui/spinner';
 import TopicTreeHeader from "./TopicTreeHeader.js";
-import { useHistory } from "react-router-dom";
+
+function FormModal({buttonText, modalName, topicGroup}) {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const [input, setInput] = useState("")
+    const toast = useToast()
+
+    return (
+        <Box>
+            <Button bg="blue.400" color="white" _hover={{color:"black", bg:"blue.100"}} onClick={onOpen}>{buttonText}</Button>
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay/>
+                <ModalContent>
+                    <ModalHeader>Add {modalName}</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <Text>{modalName} Name</Text>
+                        <InputGroup>
+                            <Input onChange={e => setInput(e.target.value)}></Input>
+                        </InputGroup>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button bg="blue.400" color="white" _hover={{color:"black", bg:"blue.100"}} onClick={() => {
+                            if (input === "") {
+                                return toast({
+                                    title: "Error",
+                                    description: "Please specify the name",
+                                    status: "error",
+                                    duration: 3000,
+                                    isClosable: true,
+                                })
+                            }
+
+                            if (modalName === "Topic Group") {
+
+                            }
+                            else if (modalName === "Topic") {
+                                fetch(post_new_topic_url(topicGroup, input), {method: "POST"}).then(e => {
+                                    if (e.status === 200) {
+                                        window.location.reload()
+
+                                        return toast({
+                                            title: "Success",
+                                            status: "success",
+                                            duration: 3000,
+                                            isClosable: true,
+                                        })
+                                    }
+                                    else {
+                                        return toast({
+                                            title: "Error",
+                                            description: "Please try again",
+                                            status: "error",
+                                            duration: 3000,
+                                            isClosable: true,
+                                        })
+                                    }
+                                })
+                            }
+                        }}>ADD</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </Box>
+    )
+}
+
+function TopicModal({topicGroup, topicName}) {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    return (
+        <Box>
+            <Flex key={topicGroup + "-topic-" + topicName } justifyContent="flex-start" padding={5} cursor="pointer" _hover={{bg:"gray.100", fontWeight:"medium"}} onClick={onOpen}>
+                <Flex width="100%">
+                    <Text>
+                        {topicName}
+                    </Text>
+                </Flex>
+            </Flex>
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay/>
+                <ModalContent>
+                    <ModalHeader>{topicGroup}</ModalHeader>
+                    <ModalCloseButton/>
+                    <ModalBody>
+                        <Text fontWeight="medium">{topicName}</Text>
+                        Honestly I dont know what to put here so :D
+                    </ModalBody>
+                    <ModalFooter>
+
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </Box>
+    )
+}
 
 export default function TopicTreeList() {
     const [data, setData] = useState([]);
     const [display, setDisplay] = useState([])
     const [view, setView] = useState("List View");
+    const [ form, setForm ] = useState({"topic_group": null, "topic": null})
+    const toast = useToast()
 
     useEffect(async function () {
         fetch(topic_group_url)
@@ -46,7 +149,7 @@ export default function TopicTreeList() {
             <TopicTreeHeader id="topic-tree-header" view={view}></TopicTreeHeader>
             <Flex paddingBlock={10} justifyContent="center" flexDirection="column">
                 <Flex justifyContent="center">
-                    <Flex width="50%">
+                    <Flex width={["95%", "95%", "90%", "70%"]}>
                         <InputGroup variant="filled" alignSelf="center">
                             <InputLeftElement pointerEvents="none" children={<SearchIcon color="gray.300" />}/>
                             <Input placeholder="Search" onChange={e => {
@@ -66,20 +169,20 @@ export default function TopicTreeList() {
                                 setDisplay(tempArray)
                             }}></Input>
                         </InputGroup>
-                        <Button bg="blue.400" color="white" _hover={{color:"black", bg:"blue.100"}} marginLeft={5}>
-                            ADD GROUP
-                        </Button>
+                        <Box marginLeft={5}>
+                        <FormModal buttonText="ADD TOPIC" modalName="Topic Group"></FormModal>
+                        </Box>
                     </Flex>
                 </Flex>
                 <Flex justifyContent="center" marginTop={10}>
-                    <Accordion allowMultiple width="50%">
+                    <Accordion allowMultiple width={["100%", "100%", "90%", "70%"]}>
                         {display.map(e => {
                             return (
                                 <AccordionItem key={"topic-group-" + e.id}>
                                     <AccordionButton as={Flex} bg="white" cursor="pointer">
                                         <Flex flexDirection={["column", "column", "row"]}>
                                             <Box marginRight={20} textOverflow="ellipsis">
-                                                <Heading fontSize="lg" width="200px" overflow="hidden" height="20px" textOverflow="ellipsis" whiteSpace="nowrap">
+                                                <Heading fontSize="lg" width="200px" overflow="hidden" textOverflow="ellipsis" whiteSpace="nowrap">
                                                     {e.name}
                                                 </Heading>
                                             </Box>
@@ -97,20 +200,14 @@ export default function TopicTreeList() {
                                         {
                                             e.topics_list.map(topic => {
                                                 return (
-                                                    <Flex key={e.name + "-topic-" + topic.name } justifyContent="flex-start" padding={5} _hover={{bg:"gray.100", fontWeight:"medium"}}>
-                                                        <Flex width="100%" cursor="pointer">
-                                                            <Text>
-                                                                {topic.name}
-                                                            </Text>
-                                                        </Flex>
-                                                    </Flex>
+                                                    <TopicModal topicGroup={e.name} topicName={topic.name}></TopicModal>
                                                 )
                                             })
                                         }
                                         </Stack>
                                         <Flex>
                                             <Flex flexGrow={1}></Flex>
-                                            <Button bg="blue.400" color="white" _hover={{color:"black", bg:"blue.100"}}>ADD TOPIC</Button>
+                                            <FormModal buttonText="ADD TOPIC" modalName="Topic" topicGroup={e.name}></FormModal>
                                         </Flex>
                                     </AccordionPanel>
                                 </AccordionItem>
