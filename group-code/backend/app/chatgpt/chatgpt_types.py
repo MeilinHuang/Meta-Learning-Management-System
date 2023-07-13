66a1f79043dabaaaf543415d4d1d58975779cfba
Fable <8vefable@gmail.com>
2023-07-13 12:34:18 +1000

    Add comment

--- group-code/backend/app/chatgpt/chatgpt_types.py
+++ group-code/backend/app/chatgpt/chatgpt_types.py
@@ -3,5 +3,8 @@ from typing import TypedDict, Literal
 Role = Literal["user", "system", "assistant"]
 
 class Message(TypedDict):
+    """
+    Represents a message in the OpenAI /chat/completions API.
+    """
     role: Role
     content: str
